8d25f1e88e3e842e8cc993baed54f73cb9d7f2b5
Gih <giovannabreinackcolombara@gmail.com>
2025-06-18 22:39:10 -0300

    configuração de banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234567',
+      database: 'db_blogpessoal',
+      entities: [],
+      synchronize: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
