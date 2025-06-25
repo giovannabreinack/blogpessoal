ed8c1f8b7126d6d4a5d59699b38ae9563e11a999
Gih <giovannabreinackcolombara@gmail.com>
2025-06-25 11:02:29 -0300

    adicionando dependencias nas classes

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -2,12 +2,14 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
+import { TemaService } from "src/tema/services/tema.service";
 
 @Injectable()
 export class PostagemService {
     constructor(
         @InjectRepository(Postagem)
-        private postagemRepository: Repository<Postagem>
+        private postagemRepository: Repository<Postagem>,
+        private temaService:TemaService
     ) { }
 
     async findAll(): Promise<Postagem[]> {
@@ -46,11 +48,14 @@ export class PostagemService {
     }
 
     async create(postagem: Postagem): Promise<Postagem> {
+        await this.temaService.findById(postagem.tema.id) 
         return await this.postagemRepository.save(postagem);
     }
 
     async update(postagem: Postagem): Promise<Postagem> {
         await this.findById(postagem.id)
+
+        await this.temaService.findById(postagem.tema.id)
 
         return await this.postagemRepository.save(postagem);
     }
